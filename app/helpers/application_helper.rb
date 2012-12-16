94497154082ee8dbeb9ac29b3866ff26e9c8c6f5
logube <m253ou@gmail.com>
2012-12-16 20:00:05 +0100

    added a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # title helper in case the @title was empty
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
